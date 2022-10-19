6e06f9189ab574ec6bc06e33a434b0a63b4929bb
coderstr <3420580499@qq.com>
2022-10-19 11:23:54 +0800

    use prettierrc

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue"
+import { createPinia } from "pinia"
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue"
+import router from "./router"
 
-import './assets/main.css'
+import "./assets/main.css"
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app.mount("#app")
 console.log(import.meta.env.VITE_APP_TITLE)
