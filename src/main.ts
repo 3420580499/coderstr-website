72e472d094a8712ffaff3f4ac7af7db2d97e66fb
coderstr <3420580499@qq.com>
2022-10-19 13:46:07 +0800

    项目基础依赖

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { createPinia } from "pinia"
 
 import App from "./App.vue"
 import router from "./router"
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount("#app")
-console.log(import.meta.env.VITE_APP_TITLE)
