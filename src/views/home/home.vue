e68d92baa2e494cb86f7f9b2a5b4d38180529eb0
coderstr <3420580499@qq.com>
2022-10-19 17:40:18 +0800

    unocss的基本使用

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="home">home</div>
+  <div class="home">
+    <button
+      class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700 border-none cursor-pointer"
+    >
+      Click me
+    </button>
+    <button
+      p="y-2 x-4"
+      font="semibold"
+      shadow="lg"
+      text="white"
+      bg="green-500 hover:green-700"
+      border="rounded-lg none "
+      cursor="pointer"
+    >
+      Click me
+    </button>
+    <div class="i-ic-baseline-account-circle bg-green-500 text-xl"></div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,22 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueJsx from "@vitejs/plugin-vue-jsx"
 
+import Unocss from "unocss/vite"
+import { presetUno, presetAttributify, presetIcons } from "unocss"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Unocss({
+      // unocss相当于是集成了tailwindcss和windicss
+      // unocss同样支持定制化
+      // 这里先使用3个预设
+      // 第一个是工具类预设，第二个是属性化模式支持，第三个是icon支持
+      presets: [presetUno(), presetAttributify(), presetIcons()]
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
